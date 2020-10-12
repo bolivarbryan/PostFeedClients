6c040ed4520f2adf28dd6d4107343b37a36ede64
Bryan Bolivar <bryan.andres.bolivar@gmail.com>
2020-10-12 11:16:07 -0500

    Created SwiftUI Project

---
+++ iOS-SwiftUI/PostFeedClient-SwiftUI/PostFeedClient-SwiftUI/PostFeedClient_SwiftUIApp.swift
@@ -0,0 +1,20 @@
+//
+//  PostFeedClient_SwiftUIApp.swift
+//  PostFeedClient-SwiftUI
+//
+//  Created by Bryan A Bolivar M on 10/12/20.
+//
+
+import SwiftUI
+
+@main
+struct PostFeedClient_SwiftUIApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
